0a5d3b6630cbb48ea34238f6e055e230ddfd0122
Jeremy Rapich <jeremysr@protonmail.com>
2024-11-01 08:43:44 -0600

    simple looping function based on trip type

--- index.js
+++ index.js
@@ -1,17 +1,43 @@
 /**
- * Calculate the percentage of missed headways as a portion of all affected trips 
+ * Calculate the percentage of missed headways as a portion of all affected trips
  * @param {object} trips contains 3 arrays, 1 each for NTstops, SLCstops, normalstops
  * @returns {number} sum of all percentages of total affected trips
  */
 
 const headwayCalc = (trips) => {
-    const SLCstops = 8;
-    const NTstops = 10;
-    const stops = 16;
-    const percentages = [];
-    let total;
+  const SLCstops = 8;
+  const NTstops = 10;
+  const stops = 16;
+  const percentages = [];
+  let total;
 
-
-}
+  /**
+   * for loop function to iterate over trips arrays. array length of 0 means no trips for that type.
+   * pushes to percentages array
+   * @param {array} trips an array of trips
+   * @param {string} type  type of trip to loop, slc, nt, or normal trips. default of normal trip unless specified
+   * @returns nothing
+   */
+  const tripLoop = (trips, type) => {
+    switch (type) {
+        case "slc":
+            for (let i = 0; i < trips.length; i++) {
+                percentages.push(trips[i] / SLCstops);
+            }
+            break;
+        case "nt":
+            for (let i = 0; i < trips.length; i++) {
+                percentages.push(trips[i] / NTstops);
+            }
+            break;
+        default:
+            for (let i = 0; i < trips.length; i++) {
+                percentages.push(trips[i] / stops);
+            }
+            break;
+    }
+    return;
+  };
+};
 
 headwayCalc();
