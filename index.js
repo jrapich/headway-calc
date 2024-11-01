1341ac331059c5dc1f6b200ee31ad6ddc716f173
Jeremy Rapich <jeremysr@protonmail.com>
2024-11-01 08:16:05 -0600

    initial function and variables

---
+++ index.js
@@ -0,0 +1,17 @@
+/**
+ * Calculate the percentage of missed headways as a portion of all affected trips 
+ * @param {object} trips contains 3 arrays, 1 each for NTstops, SLCstops, normalstops
+ * @returns {number} sum of all percentages of total affected trips
+ */
+
+const headwayCalc = (trips) => {
+    const SLCstops = 8;
+    const NTstops = 10;
+    const stops = 16;
+    const percentages = [];
+    let total;
+
+
+}
+
+headwayCalc();
